5b242d0864fa7cef702d9306264f340de236b4ff
Mariusz12345 <przybylowski303@gmail.com>
2015-07-30 11:22:36 +0200

    Projekcik

--- ProjektIndeks/ProjektIndeks-ejb/src/main/java/projekt/Student.java
+++ ProjektIndeks/ProjektIndeks-ejb/src/main/java/projekt/Student.java
@@ -1,5 +1,33 @@
 package projekt;
 
-public class Student {
+import javax.persistence.Column;
+
+import javax.persistence.Entity;
+
+
+
+@Entity
+public class Student extends Osoba {
+	
+	
+	@Column(name = "nrAlbumu")
+	private Long nrAlbumu;
+
+	public Long getNrAlbumu() {
+		return nrAlbumu;
+	}
+
+	public void setNrAlbumu(Long nrAlbumu) {
+		this.nrAlbumu = nrAlbumu;
+	}
+
+	public Student(Long id_studenta, String imie, String nazwisko, Long nrAlbumu) {
+		super(id_studenta, imie, nazwisko);
+		this.nrAlbumu = nrAlbumu;
+	}
+	
+	
+	
+	
 
 }
