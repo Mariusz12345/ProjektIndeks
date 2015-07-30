3fcfc0d7fdd88c7cf52d1c56393cd11ffceb3b38
Mariusz12345 <przybylowski303@gmail.com>
2015-07-30 13:52:03 +0200

    Projekcik

--- ProjektIndeks/ProjektIndeks-ejb/src/main/java/projekt/Ocena.java
+++ ProjektIndeks/ProjektIndeks-ejb/src/main/java/projekt/Ocena.java
@@ -1,9 +1,31 @@
 package projekt;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
-
-
-
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "zaliczenieRoku", discriminatorType = DiscriminatorType.STRING)
+@Table(name = "ocena", schema = "indeks")
 public class Ocena {
+
+	@Id
+	@SequenceGenerator(name = "id_zaliczenia_seq", sequenceName = "indeks.id_zaliczenia_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_zaliczenia_seq")
+	private Long id_zaliczenia;
+	
+	@Column(name = "wpisDoIndeksu")
+	private String wpisDoIndeksu;
+
+	
 
 }
